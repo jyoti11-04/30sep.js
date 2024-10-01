b41c5dec79af435bd7de6852e1bd07300193b208
jyoti11-04 <jyotipal110404@gmail.com>
2024-10-01 16:44:52 +0530

    30sep.js

---
+++ 30sep.js
@@ -0,0 +1,11 @@
+var date =  new Date()
+console.log(date.toString());
+var new_date = new Date("2002/12/14")
+console.log(new_date);
+var arr = ["ankit","vivek","sam"]
+arr.unshift("abc")
+console.log(arr);
+arr.pop()
+console.log(  );
+console.log(arr.includes("vivek"));
+console.log(arr.indexOf("vivek"));
